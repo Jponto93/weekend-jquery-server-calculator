9af421d2b16198fe7ecd62cfd75093cd90fe8ab3
Jordan Ponto <jponto93@gmail.com>
2021-10-09 17:27:52 -0500

    math logic works, cant get front and back end connected

--- server/public/client.js
+++ server/public/client.js
@@ -4,5 +4,59 @@ $(ready);
 
 function ready () {
     console.log('JQ Ready');
-    
+    $('.opBtn').on('click', addMathOperator)
+    $('#submitBtn').on('click', submitCalculation)
 } // ready
+
+let operator = '';
+let opData = 0;
+
+function addMathOperator () {
+    console.log('inside addMathOperator');
+    //log the operator clicked
+    console.log('this.html', $(this).html());
+    //assign operator clicked to operator variable
+    opData = $(this).data();
+    console.log(opData);
+    
+    return operator = $(this).html()
+    
+}// addMathOperator
+
+
+
+function submitCalculation () {
+    console.log('inside submitCalculation');
+    let num1 = $('#firstNumber').val();
+    let num2 = $('#secondNumber').val();
+    console.log(`First number: ${num1}, op is ${operator}, Second number: ${num2}`);
+    $.ajax({
+        method: 'POST',
+        url: '/calculate',
+        data: {
+            firstNumber: num1,
+            secondNumber: num2,
+            operator: operator,
+        }
+    }).then(function (response){
+        console.log('Successful POST', response)
+
+    }).catch(function (response){
+        alert('POST FAILED', response)
+    })
+}; // end submitCalculation
+
+
+function getCalculations () {
+    $.ajax({
+        method: 'GET',
+        url: '/calculate',
+    }).then(function (response) {
+        console.log('success', response);
+
+    }).catch(function () {
+        alert('Get failed getCalculation')
+    });
+}
+
+
--- server/server.js
+++ server/server.js
@@ -3,19 +3,57 @@ const express = require('express');
 
 const app = express();
 
- const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
+
+const data = require('./modules/data')
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const PORT = 5000;
+
+app.use(express.static('../server/public/'));
 
 app.listen(PORT, () => {
     console.log('listening on port', PORT);
 });
 
-// const data = module.require('./server/modules/data.js')
+app.get('/calculate', (req, res) => {
+    res.send(data);
+})
 
-// let mathObj = {
-//     firstNum: '',
-//     secondNum: '',
-//     operator: '',
-// }
+app.post('/calculate', (req, res) => {
+    console.log('inside server side calculate POST');
+    // answer is where we store our answer...
+    let answer = 0;
+    // let problem = data sent from client
+    let problem = req.body;
+    console.log(req.body);
+    
+    switch(problem.operator){
+        case '+': 
+            answer = Number(problem.firstNumber) + Number(problem.secondNumber);
+        break;
+
+        case '-': 
+            answer = Number(problem.firstNumber) - Number(problem.secondNumber);
+        break;
+
+        case '*':
+            answer = Number(problem.firstNumber) * Number(problem.secondNumber);
+        break;
+
+        case '/':
+            answer = Number(problem.firstNumber) / Number(problem.secondNumber);
+        break;
+        
+    }
+    console.log(answer);
+    
+
+    // push the problem into data (storage)
+    data.push(problem);
+    console.log('this is our data array', data);
+
+
+    res.sendStatus(201);
+})
