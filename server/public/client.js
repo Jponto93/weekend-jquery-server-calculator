61a13da59e30d43a5aaa1b44b642f1d76a730785
Jordan Ponto <jponto93@gmail.com>
2021-10-10 13:54:59 -0500

    all inputs required to submit calculation

--- server/public/client.js
+++ server/public/client.js
@@ -2,67 +2,78 @@ console.log('hello from client js');
 
 $(ready);
 
-function ready () {
+function ready() {
     console.log('JQ Ready');
     $('.opBtn').on('click', addMathOperator)
     $('#submitBtn').on('click', submitCalculation)
+    $('#clearBtn').on('click', clearInputs)
 } // ready
 
 let operator = '';
 let opData = 0;
 
+function clearInputs() {
+    console.log('in clearInputs');
+    $('#firstNumber').val('');
+    $('#secondNumber').val('');
+    operator = '';
+} // end clearInputs
 
-function addMathOperator () {
+
+function addMathOperator() {
     console.log('inside addMathOperator');
     //log the operator clicked
     console.log('this.html', $(this).html());
     //assign operator clicked to operator variable
     // opData = $(this).data();
     // console.log(opData);
-    
+
     return operator = $(this).html()
-    
+
 }// addMathOperator
 
 
 
-function submitCalculation () {
+function submitCalculation() {
     console.log('inside submitCalculation');
     let num1 = $('#firstNumber').val();
     let num2 = $('#secondNumber').val();
     console.log(`First number: ${num1}, op is ${operator}, Second number: ${num2}`);
-
-    $.ajax({
-        method: 'POST',
-        url: '/calculate',
-        data: {
-            firstNumber: $('#firstNumber').val(),
-            secondNumber: $('#secondNumber').val(),
-            operator: operator,
-        }
-    }).then(function (response) {
-        console.log('successful POST', response)
-        getCalculations();
-        //clear number inputs
-        $('#firstNumber').val('');
-        $('#secondNumber').val('');
-    }).catch(function (response) {
-        alert ('POST Failed')
-    })
+    if (num1 && num2 && operator) {
+        $.ajax({
+            method: 'POST',
+            url: '/calculate',
+            data: {
+                firstNumber: $('#firstNumber').val(),
+                secondNumber: $('#secondNumber').val(),
+                operator: operator,
+            }
+        }).then(function (response) {
+            console.log('successful POST', response)
+            getCalculations();
+            //clear number inputs
+            $('#firstNumber').val('');
+            $('#secondNumber').val('');
+        }).catch(function (response) {
+            alert('POST Failed')
+        })
+    } else if (!num1 || !num2 || !operator) {
+        alert('Missing input')
+    }
 }
 
 
 
 
-function getCalculations () {
+function getCalculations() {
     console.log('inside get calculations');
-    
+
     $.ajax({
         method: 'GET',
         url: '/calculate',
     }).then(function (response) {
         console.log('success', response);
-        let answer = response[response.length-1].answer;
+        let answer = response[response.length - 1].answer;
         console.log(answer);
         renderAnswerToDom(answer);
         renderHistoryToDom(response);
@@ -73,13 +84,14 @@ function getCalculations () {
     });
 }
 
-function renderAnswerToDom(number){
+function renderAnswerToDom(number) {
     console.log('inside renderAnswerToDom');
-    
-    $('#answer').empty();
-    $('#answer').append(
-    `
-    ${number}
+
+    $('#answerDisplay').empty();
+    $('#answerDisplay').append(`
+    <h2 id="answer">
+        ${number}
+    </h2>
     `);
 } // end renderToDom
 
@@ -87,7 +99,7 @@ function renderHistoryToDom(answers) {
     console.log('inside renderHistoryToDom');
     let el = $('#history');
     el.empty();
-    for (let answer of answers){
+    for (let answer of answers) {
         el.append(`
         <ul>
             <li>${answer.firstNumber} ${answer.operator} ${answer.secondNumber} = ${answer.answer}</li>
